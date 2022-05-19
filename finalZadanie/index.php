5d3180065b8324069335a788255949cbf985a856
Dávid Kurek <xkurek@stuba.sk>
2022-05-19 12:37:14 +0200

    Uvod

--- finalZadanie/index.php
+++ finalZadanie/index.php
@@ -1,12 +1,16 @@
 <?php
-$x = 5 + 5;
+$x = $_POST["r"];
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 
-<h1>Nadpis</h1>
-<p>Paragraf.</p>
+<h1>Zaverecne zadanie</h1>
+<form action="index.php" method="post">
+    <label for="r">:</label><br>
+    <input type="number" id="r" name="r" value="0.2"><br>
+    <input type="submit" value="Submit">
+</form>
 <?php
 echo $x;
 ?>
