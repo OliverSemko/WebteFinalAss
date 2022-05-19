946b246351581cf5848224ac59f57d1410201af9
MarianChoma <xchoma@stuba.sk>
2022-05-19 13:34:26 +0200

    My first commit test h1 title

--- finalZadanie/index.php
+++ finalZadanie/index.php
@@ -21,6 +21,7 @@ $x = $_POST["r"];
 <!--        <input type="text" id="command" name="command">-->
         <input type="submit" value="Submit" class="btn btn-primary">
     </form>
+    <h1>CAVKO</h1>
     <div class="canvas">
         <canvas id="myCanvas"></canvas>
         <canvas id="CanvasChart"></canvas>
