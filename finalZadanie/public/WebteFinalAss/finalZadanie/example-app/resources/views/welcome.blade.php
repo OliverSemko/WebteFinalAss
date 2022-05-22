b524ce7705f862dd11d44ca54fb4fe6de79a7f48
OliverSemko <xsemko@stuba.sk>
2022-05-22 17:39:57 +0200

    commit save

---
+++ finalZadanie/public/WebteFinalAss/finalZadanie/example-app/resources/views/welcome.blade.php
@@ -0,0 +1,60 @@
+<?php
+error_reporting(0);
+ini_set('display_errors', 0);
+$x = $_POST["r"];
+?>
+    <!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Finalne zadanie</title>
+    <link rel="icon" type="image/x-icon" href="https://icon-library.com/images/icon-favicon/icon-favicon-19.jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+
+<h1 id="nadpis">Záverečné zadanie</h1>
+<div id="flag" onclick="flag()">
+</div>
+<div class="container">
+    <form action="welcome.blade.php" method="post">
+        <label for="command" id="label_prikaz">Zadajte príkaz:</label><br>
+        <textarea id="command" name="command" placeholder="Príkaz..."></textarea><br>
+        <input type="submit" value="Potvrď" id="input_submit" class="btn btn-primary">
+    </form>
+    <div class="form-check form-check-inline">
+        <input type="checkbox" id="vehicle" name="vehicle" value="Car" onclick="foo()">
+        <label for="vehicle" id="car_label">Zobrazenie auta</label><br>
+    </div>
+    <div class="form-check form-check-inline">
+        <input type="checkbox" id="chart" name="chart" value="Chart" onclick="foo()" checked>
+        <label for="chart" id="graph_label">Zobrazenie grafu</label><br>
+    </div>
+    <div class="canvas">
+        <canvas id="carCanvas">
+        </canvas>
+        <canvas id="CanvasChart"></canvas>
+    </div>
+</div>
+
+<?php
+echo $x;
+?>
+<footer>
+    <p>&copy; Oliver Semko, Marián Choma, Jakub Szabo, Dávid Kurek </p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js"
+        integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js"
+        integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+<script src="../js/scriptChart.js"></script>
+<script src="../js/main.js"></script>
+<script src="../js/script.js"></script>
+</body>
+</html>
