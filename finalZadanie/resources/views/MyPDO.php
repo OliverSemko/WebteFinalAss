873ef680f26e3056f2f3b1548e2178ea80e5d1f0
MarianChoma <xchoma@stuba.sk>
2022-05-24 12:55:28 +0200

    database

---
+++ finalZadanie/resources/views/MyPDO.php
@@ -0,0 +1,46 @@
+<?php
+
+
+class MyPDO
+{
+    protected static $instance;
+    protected $pdo;
+
+    public function __construct()
+    {
+        //new MyPDO("mysql:host=localhost;dbname=zad5", "rabek", "secret");
+        $opt = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => FALSE,
+        );
+        $dsn = 'mysql:host=localhost;dbname=FinalneZadanie';
+        $this->pdo = new PDO($dsn, "xchoma", "sRknjGTdYiJITdI2000.", $opt);
+
+    }
+
+    // a classical static method to make it universally available
+    public static function instance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array(array($this->pdo, $method), $args);
+    }
+
+    public function run($sql, $args = [])
+    {
+        if (!$args) {
+            return $this->query($sql);
+        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+}
+
--- finalZadanie/resources/views/main.php
+++ finalZadanie/resources/views/main.php
@@ -1,10 +1,12 @@
-<?php include 'header-footer/header.php'?>
+
+<?php include 'header-footer/header.php' ?>
 
 <div class="container">
     <form action="welcome.blade.php" method="post">
         <label for="command" id="label_prikaz">Zadajte príkaz:</label><br>
         <textarea id="command" name="command" placeholder="Príkaz..."></textarea><br>
-        <input onclick="getData(event)" type="submit" value="Potvrď" id="input_submit" class="btn btn-primary">
+        <input onclick="getData(event)" type="submit" value="Potvrď" name="Potvrď" id="input_submit"
+               class="btn btn-primary">
     </form>
     <div class="form-check form-check-inline">
         <input type="checkbox" id="vehicle" name="vehicle" value="Car" onclick="foo()">
@@ -20,5 +22,24 @@
         <canvas id="CanvasChart"></canvas>
     </div>
 </div>
+<?php
+require_once "MyPdo.php";
+if (isset($_POST["Potvrď"]) && isset($_POST["command"])) {
+    try {
+        $myPdo = new MyPDO();
+        $myPdo->run("INSERT into measurement
+            (`date`, `r`, `error`) values (?, ?, ?)",
+            [date('d-m-y h:i:s'), $_POST["command"], false]);
+
+    } //catch exception
+    catch (Exception $e) {
+        $myPdo = new MyPDO();
+        $myPdo->run("INSERT into measurement
+            (`date`, `r`, `error`) values (?, ?, ?)",
+            [date('d-m-y h:i:s'), $_POST["command"], false]);
+    }
+}
+
+?>
 
 <?php include 'header-footer/footer.php' ?>
